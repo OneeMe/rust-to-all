953b4c98f831914ac665ef7f6dc85e8ff4220c74
onee <onee.me@outlook.com>
2022-03-11 19:53:05 +0800

    fix: fix rn activity component name bug

--- android/app/src/main/java/com/rn/ReactNativeHostActivity.java
+++ android/app/src/main/java/com/rn/ReactNativeHostActivity.java
@@ -5,9 +5,12 @@ import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 
 public class ReactNativeHostActivity extends ReactActivity {
-
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
   @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
+  protected String getMainComponentName() {
+    return "rn";
   }
 }
