ff5ccc8956ed771b4f89a7d5d1ff4db3d3ab8325
onee <onee.me@outlook.com>
2022-03-11 15:48:26 +0800

    chore: add react native

---
+++ App.js
@@ -0,0 +1,28 @@
+
+
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <View
+        style={styles.item}
+      ></View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  item: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'red',
+  },
+});
+
+export default App;
